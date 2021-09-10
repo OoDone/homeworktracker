5126f9663765df7cbee875852fc3d3df358239e2
OoDone <48130949+OoDone@users.noreply.github.com>
2021-09-10 10:06:38 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-
+var http = require('http'),
+    https = require('https');
+var fs = require('fs');
+var cluster = require('./cluster.js');
+var body = ''
+var server = http.createServer(function(req, res) {
+  request.on('data', function(data) {
+      body += data
+      console.log('Partial body: ' + body)
+  })
+  request.on('end', function() {
+    console.log('Body: ' + body)
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    response.end('post received')
+  })
+});
