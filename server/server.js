d6c0bb2a6badd99b1ce04f9266468318c5bf48f3
OoDone <48130949+OoDone@users.noreply.github.com>
2021-09-10 12:03:24 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,15 +2,16 @@ var http = require('http'),
     https = require('https');
 var body = ''
 var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-  request.on('data', function(data) {
+    req.on('error', (err) => {
+        console.error(err);
+    })
+  req.on('data', function(data) {
       body += data
       console.log('Partial body: ' + body)
   })
-  request.on('end', function() {
+  req.on('end', function() {
     console.log('Body: ' + body)
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    response.end('post received')
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end('post received')
   })
-});
-server.listen(8080 || process.env.PORT);
+}).listen(8080 || process.env.PORT);
